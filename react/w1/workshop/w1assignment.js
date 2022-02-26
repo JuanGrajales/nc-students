d59c069e6e8f065ec30ce0a20f30ab50c59b0607
Juan Grajales <juan@Juans-Macbook-Pro-Work.local>
2022-02-26 13:04:59 -0500

    task 2 week 1

--- react/w1/workshop/w1assignment.js
+++ react/w1/workshop/w1assignment.js
@@ -12,9 +12,31 @@ class Bootcamp {
     this.level = level;
     this.students = students;
   }
+
+  registerStudent(newStudent){
+      console.log(newStudent)
+      // check if the new student email already existing in the current students
+        const duplicateStudentsFound = this.students.filter((existingStudent) => {
+            if(existingStudent.email === newStudent.email){
+                return true
+            }
+        })
+
+      // if the student is not registered then add to array of students
+      if(duplicateStudentsFound.length === 0){
+          this.students.push(newStudent)
+      } else { // else show message that student is already registered
+          console.log( `${newStudent.email} is already registered`)
+      }
+  }
 }
 
 const s1 = new Student('Ashley', 'a@a.com', 'TN')
 const s2 = new Student('Marshal', 'm@m.com', 'CT')
 const s3 = new Student('Belinda', 'b@b.com', 'TN')
-const b1 = new Bootcamp('React', 1, [s1])
+const b1 = new Bootcamp('React', 1)
+
+b1.registerStudent(s1)
+b1.registerStudent(s2)
+b1.registerStudent(s3)
+b1.registerStudent(s1)
