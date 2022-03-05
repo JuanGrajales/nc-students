5ba291889d77bd4fc70ba219c99562fca350e414
Juan Grajales <inbox1.juan+github@gmail.com>
2022-03-05 12:42:55 -0500

    task 3 partial

--- react/w2/workshop/nucampsite/src/components/CampsiteInfoComponent.jsx
+++ react/w2/workshop/nucampsite/src/components/CampsiteInfoComponent.jsx
@@ -16,11 +16,53 @@ class CampsiteInfoComponent extends Component {
     );
   }
 
+  renderComments = (comments) => {
+    if (comments) {
+      //   const displayComments = comments.map((comment) => {
+      //     return (
+      //       <div>
+      //         <p>{comment.text}</p>
+      //         -- {comment.author}, {comment.date}
+      //       </div>
+      //     );
+      //   });
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => {
+            return (
+              <div>
+                <p>{comment.text}</p>
+                -- {comment.author}, {comment.date}
+              </div>
+            );
+          })}
+          {/* {displayComments} */}
+          {/* <p>{comments[0].text}</p>
+          -- {comments[0].author}, {comments[0].date} */}
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+
+    // return <div>
+    //     {comments ? <div>display comments</div> : ''}
+    // </div>
+  };
+
   render() {
     // if(this.props.campsite !== null){
+
     if (this.props.campsite) {
+      console.log(this.props.campsite.comments);
+      console.log(this.props.campsite.comments[0]);
+      console.log(this.props.campsite.comments[0].author);
       return (
-        <div className="row">{this.renderCampsite(this.props.campsite)}</div>
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
+        </div>
       );
     } else {
       return (
